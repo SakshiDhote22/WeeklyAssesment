c0cac74ceee6a0cef5375c60c046f1205dbbab6f
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-08-16 10:41:04 +0530

    Update ListMapping.java

--- 16082022jpa/main/java/com/collection/ListMapping.java
+++ 16082022jpa/main/java/com/collection/ListMapping.java
@@ -1,4 +1,11 @@
 
+/Develop an application using JPA
+
+try to insert employee Information(empno,name,designation,sal ..etc) into database
+
+and also fetch all the data from the database then display on console
+*/
+
 package com.java.collection;
 import javax.persistence.*;
 
